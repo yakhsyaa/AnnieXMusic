4fc026ab7cd6a268fe7a32061d5a3468dc402bc4
yakhsyaa <240030283@stikom-bali.ac.id>
2025-10-04 09:29:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -76,15 +76,15 @@ STICKERS = [
     "CAACAgUAAx0Cd6nKUAACASBl_rnalOle6g7qS-ry-aZ1ZpVEnwACgg8AAizLEFfI5wfykoCR4h4E",
     "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ",
 ]
-HELP_IMG_URL = "https://files.catbox.moe/yg2vky.jpg"
-PING_VID_URL = "https://files.catbox.moe/3ivvgo.mp4"
+HELP_IMG_URL = "https://files.catbox.moe/0jbayc.png"
+PING_VID_URL = "https://files.catbox.moe/awk4k3.jpg"
 PLAYLIST_IMG_URL = "https://files.catbox.moe/yhaja5.jpg"
 STATS_VID_URL = "https://telegra.ph/file/e2ab6106ace2e95862372.mp4"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/mlztag.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/tiss2b.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/1d3da7.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/zhymxl.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/veykzq.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/0jbayc.png"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/0jbayc.png"
+STREAM_IMG_URL = "https://files.catbox.moe/0jbayc.png"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/0jbayc.png"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/0jbayc.png"
 SPOTIFY_ARTIST_IMG_URL = SPOTIFY_ALBUM_IMG_URL = SPOTIFY_PLAYLIST_IMG_URL = YOUTUBE_IMG_URL
 
 # ── Helpers ────────────────────────────────────────────────────────────────────
